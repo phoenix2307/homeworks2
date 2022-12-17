4c26739fa6a450dd6b8ba9b7a528e0f70f84d762
Alex <phoenix.trade2307@gmail.com>
2022-12-17 13:13:59 +0200

    HW2 finished

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { AffairType } from '../../HW2'
+import {AffairType} from '../../HW2'
 import s from './Affair.module.css'
 import s2 from '../Affairs.module.css'
 
 type AffairPropsType = {
     // key не нужно типизировать
     affair: AffairType
-    deleteAffairCallback: any // need to fix any
+    deleteAffairCallback: () => void // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
+
     const deleteCallback = () => {
+        props.deleteAffairCallback()
         // need to fix
     }
 
@@ -19,15 +21,15 @@ function Affair(props: AffairPropsType) {
     const affairClass = s.affair + ' ' + s2[props.affair.priority]
 
     return (
-        <div
-            id={'hw2-affair-' + props.affair._id}
-            className={affairClass}
-        >
+        <div id={'hw2-affair-' + props.affair._id}
+            className={affairClass}>
+
             <div id={'hw2-name-' + props.affair._id} className={nameClass}>
                 {/*создаёт студент*/}
-
+                {props.affair.name}
                 {/**/}
             </div>
+
             <div id={'hw2-priority-' + props.affair._id} hidden>
                 {props.affair.priority}
             </div>
@@ -35,13 +37,14 @@ function Affair(props: AffairPropsType) {
             <button
                 id={'hw2-button-delete-' + props.affair._id}
                 className={buttonClass}
+                onClick={deleteCallback}
                 // need to fix
-
             >
                 {/*текст кнопки могут изменить студенты*/}
                 X
                 {/**/}
             </button>
+
         </div>
     )
 }
