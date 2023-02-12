7b07b6faf9e5b8ce8ab1680ca223fa545be3d505
Alex <phoenix.trade2307@gmail.com>
2023-02-12 22:34:44 +0200

    hw8-logic

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,13 +4,38 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            return state.sort((a,b)=>{
+                if (action.payload === 'up'){
+                    if (a.name > b.name){
+                        return 1
+                    }
+                    if (a.name < b.name){
+                        return -1
+                    }
+                    return 0
+                }
+                if (action.payload === 'down'){
+                    if (a.name < b.name){
+                        return 1
+                    }
+                    if (a.name > b.name){
+                        return -1
+                    }
+                    return 0
+                }
+                return 0
 
-            return state // need to fix
+            })
+
+            // return newState // need to fix
         }
         case 'check': {
+            state = state.filter((u)=>{
+                return u.age > action.payload || u.age === action.payload
+            })
 
             return state // need to fix
         }
